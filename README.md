02cb3bd575fc353746608c51de42738a8ec54d74
Anton Kolesnyk <antkmsft@users.noreply.github.com>
2021-01-05 17:17:12 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Bash: Azure Storage DataLake C++ Standalone Sample Consuming Azure SDK via VcPkg
+
+This is supposed to be used only for Azure C++ team for one-off verification of VcPkg integration.
+
+It pulls and builds the Azure SDK, from my fork (https://github.com/antkmsft/azure-sdk-for-cpp/).
+The releases I have there are based on Monday Jan 1st, 2021 master branch of our repo.
+
+## Instructions
+
+* Rename your current vcpkg directory.
+* Clone antkmsft's fork of the vcpkg repo: `git clone https://github.com/antkmsft/vcpkg.git`, so that it exists where your old vcpkg directory used to be.
+* Switch to azure-sdk-for-cpp branch: `git checkout azure-sdk-for-cpp`
+* Install DataLake SDK: `vcpkg install azure-storage-files-datalake-cpp:x64-windows-static`.
+* Build this sample. All should succeed.
+
+## Further Experimentation
+
+* Try other triplets.
+* Try other platforms: mac, linux.
+* Try also installing azure-identity-cpp, azure-storage-files-shares-cpp (azure-storage-files-datalake-cpp should already bring up core, storage common, and storage blobs).
+* Let's go through a versioned dependency (modify `find_package` to specify version).
+* Let's go through an upgrade scenario.
+* Try another sample using another library. This code, for example, is an adaptation of the DataLake sample we have in our SDK.
+
+# Other
+* Let's submit a PR to VcPkg and verify that the build succeeds in their pipelines - https://github.com/microsoft/vcpkg/pull/15471
+
+## Undoing vcpkg
+* delete the vcpkg folder you cloned from my fork, rename your old vcpkg directory back to its original name.
---
+++ datalake_sample_standalone.cpp
@@ -0,0 +1,143 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+#include <cstdlib>
+#include <iostream>
+#include <stdexcept>
+#include <vector>
+
+#include <azure/storage/files/datalake.hpp>
+
+std::string GetConnectionString()
+{
+#ifdef _MSC_VER
+#pragma warning(push)
+// warning C4996 : 'getenv' : This function or variable may be unsafe.Consider using _dupenv_s instead.
+#pragma warning(disable: 4996)
+#endif // _MSC_VER
+
+    std::string envConnectionString = std::getenv("AZURE_STORAGE_CONNECTION_STRING");
+
+#ifdef _MSC_VER
+#pragma warning(pop)
+#endif // _MSC_VER
+
+    if (!envConnectionString.empty())
+    {
+        return envConnectionString;
+    }
+
+    throw std::runtime_error("Cannot find connection string");
+}
+
+int main(int argc, char** argv)
+{
+    (void)argc;
+    (void)argv;
+    // Please note that you can always reference test cases for advanced usages.
+
+    using namespace Azure::Storage::Files::DataLake;
+
+    std::string fileSystemName = "sample-file-system";
+    std::string directoryName = "sample-directory";
+    std::string fileName = "sample-file";
+
+    // Initializing a ServiceClient that can then initialize the FileSystemClient or list file
+    // systems.
+    auto serviceClient = DataLakeServiceClient::CreateFromConnectionString(GetConnectionString());
+    // Initializing a FileSystemClient that can then initialize the PathClient, FileClient,
+    // DirectoryClient.
+    auto fileSystemClient
+        = FileSystemClient::CreateFromConnectionString(GetConnectionString(), fileSystemName);
+
+    try
+    {
+        // Create file systems and ignore the already exist error.
+        try
+        {
+            fileSystemClient.Create();
+        }
+        catch (Azure::Storage::StorageException& e)
+        {
+            if (e.ErrorCode != "ContainerAlreadyExists")
+            {
+                throw;
+            }
+            else
+            {
+                std::cout << "ErrorCode: " + e.ErrorCode << std::endl;
+                std::cout << "ReasonPhrase: " + e.ReasonPhrase << std::endl;
+            }
+        }
+
+        // Create a directory.
+        auto directoryClient = fileSystemClient.GetDirectoryClient(directoryName);
+        directoryClient.Create();
+
+        // Creates a file under the directory.
+        auto fileClient = directoryClient.GetFileClient(fileName);
+        fileClient.Create();
+
+        // Append/flush/read data from the client.
+        // Append data
+        // Initialize the string that contains the first piece of data to be appended to the file.
+        std::string str1 = "Hello ";
+        // Initialize the buffer that represents what contains your data to be appended, please ignore
+        // how it is constructed here, since the memory copy is not efficient.
+        std::string str2 = "World!";
+        std::vector<uint8_t> buffer(str1.begin(), str1.end());
+
+        // One way of passing in the buffer, note that the buffer is not copied.
+        auto bufferStream = Azure::Core::Http::MemoryBodyStream(buffer);
+
+        fileClient.AppendData(&bufferStream, 0 /* Offset of the position to be appended.*/);
+
+        // Another way of passing in the buffer, note that buffer is also not copied.
+        bufferStream = Azure::Core::Http::MemoryBodyStream(
+            reinterpret_cast<const uint8_t*>(str2.data()), str2.size());
+
+        fileClient.AppendData(&bufferStream, str1.size());
+
+        // Flush
+        fileClient.FlushData(str1.size() + str2.size());
+
+        // Read
+        auto result = fileClient.Read();
+        Azure::Core::Context context;
+        std::vector<uint8_t> downloaded
+            = Azure::Core::Http::BodyStream::ReadToEnd(context, *(result->Body));
+        // downloaded contains your downloaded data.
+        std::cout << "Downloaded data was:\n" + std::string(downloaded.begin(), downloaded.end())
+            << std::endl;
+
+        // List all file systems.
+        std::string continuation;
+        std::vector<Models::FileSystem> fileSystems;
+        do
+        {
+            auto response = serviceClient.ListFileSystemsSegement();
+            if (response->ContinuationToken.HasValue())
+            {
+                continuation = response->ContinuationToken.GetValue();
+            }
+            fileSystems.insert(
+                fileSystems.end(), response->Filesystems.begin(), response->Filesystems.end());
+        } while (!continuation.empty());
+
+        // Delete file system.
+        fileSystemClient.Delete();
+
+        std::cout << "Successfully finished sample." << std::endl;
+    }
+    catch (Azure::Storage::StorageException const& e)
+    {
+        // Deal with the information when storage error is met.
+        std::cout << "Error encountered when sending the request." << std::endl;
+        std::cout << "ErrorCode: " + e.ErrorCode << std::endl;
+        std::cout << "Message: " + e.Message << std::endl;
+        std::cout << "ReasonPhrase: " + e.ReasonPhrase << std::endl;
+        std::cout << "RequestId: " + e.RequestId << std::endl;
+    }
+
+    return 0;
+}
